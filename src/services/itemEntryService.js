38c0f95e9fa96a8e2a67860029634e438f2033f8
Christine Poydence <christine.poydence@gmail.com>
2021-02-25 23:46:42 -0500

    When an administrator adds a scannable item to the POS system, it must contain a VALID name, price, and unitType

--- src/services/itemEntryService.js
+++ src/services/itemEntryService.js
@@ -1,12 +1,26 @@
 const scannableItems = {};
 
+const scannableItemSchema = {
+    itemName: value => typeof value === 'string',
+    price: value => typeof value === 'number',
+    unitType: value => ['unit', 'pounds', 'ounces', 'gallons', 'quarts', 'pints', 'cups'].includes(value)
+  };
+
+
+  const validScannableItem = (item) => {
+    const entries = Object.entries(scannableItemSchema);
+    entries.map(([key, validate]) => {
+      if(!validate(item[key])){
+        throw new Error(`Scannable items must contain a valid ${key}`)
+      }
+    });
+    return true;
+  }
+
 const addScannableItemToSystem = (item) => {
-    if(item.hasOwnProperty('item') && item.hasOwnProperty('price')){
+    if(validScannableItem(item)){
         Object.assign(scannableItems, item);
-    }else{
-        throw new Error('Scannable items must contain an item name and a price.')
     }
-   
 };
 
 module.exports = {
--- src/services/itemEntryService.spec.js
+++ src/services/itemEntryService.spec.js
@@ -5,21 +5,27 @@ const {
 
 describe('addScannableItemToSystem', () => {
 
-    test('when an item is passed to addScannableItemToSystem, it is added to the list of scannable items', () => {
-        const item = {item: "soup", price: '$1.69'}
+    test('when a valid item is passed to addScannableItemToSystem, it is added to the list of scannable items', () => {
+        const item = {itemName: "soup", unitType: 'unit', price: 1.69}
         addScannableItemToSystem(item);
         expect(scannableItems).toMatchObject(item);
     });
 
     test('when an item is passed to addScannableItemToSystem, it must contain a price', () => {
-        const item = {item: "soup"}
+        const item = {itemName: "soup", unitType: 'unit'};
         expect(() => { addScannableItemToSystem(item) }
-            ).toThrow(Error('Scannable items must contain an item name and a price.'));
+            ).toThrow(Error('Scannable items must contain a valid price'));
     });
 
     test('when an item is passed to addScannableItemToSystem, it must contain an item name', () => {
-        const item = {price: "$1.89"}
+        const item = {price: 1.89, unitType: 'pounds'}
         expect(() => { addScannableItemToSystem(item) }
-            ).toThrow(Error('Scannable items must contain an item name and a price.'));
+            ).toThrow(Error('Scannable items must contain a valid itemName'));
+    });
+
+    test('when an item is passed to addScannableItemToSystem, it must contain a valid unit type', () => {
+        const item = {itemName: 'bananas', price: 2.38}
+        expect(() => { addScannableItemToSystem(item) }
+            ).toThrow(Error('Scannable items must contain a valid unitType'));
     });
 });
