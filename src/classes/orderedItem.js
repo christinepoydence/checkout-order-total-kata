3f4b6e1c74eae08ace9156d2003c651e4211452c
Christine Poydence <christine.poydence@gmail.com>
2021-03-02 04:04:08 -0500

    refactored to use es6 classes to create an orderedItem

---
+++ src/classes/orderedItem.js
@@ -0,0 +1,9 @@
+import get from 'lodash.get';
+
+export default class OrderedItem {
+    constructor(item){
+        this.itemName = get(item, 'itemName').toLowerCase();
+        this.units = get(item, 'units');
+        this.price = +get(item,'price').toFixed(2);
+    }
+}
---
+++ src/classes/orderedItem.spec.js
@@ -0,0 +1,13 @@
+import OrderedItem from './orderedItem.js';
+
+test('OrderedItem can be constructed', () => {
+    const item = {
+        itemName: 'kiwi',
+        units: 4,
+        price: 1.42
+    }
+    const orderedItem = new OrderedItem(item);
+    expect(orderedItem.itemName).toBe('kiwi');
+    expect(orderedItem.units).toBe(4);
+    expect(orderedItem.price).toBe(1.42);
+});
