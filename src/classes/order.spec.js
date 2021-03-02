5a912c39db7748964104e0b6b628601a9ac70be9
Christine Poydence <christine.poydence@gmail.com>
2021-03-02 03:57:35 -0500

    user can add and remove items from an order

--- src/classes/order.spec.js
+++ src/classes/order.spec.js
@@ -11,3 +11,41 @@ test('instance of order is used if Order is constructed twice', () => {
     const secondOrder = new Order();
     expect(order).toEqual(secondOrder);
 });
+
+test('addOrderedItem adds an item to order.items', () => {
+    const order = new Order();
+    order.addOrderedItem({itemName: 'item'})
+    expect(order.items).toContainEqual({itemName: 'item'});
+});
+
+test('removeOrderedItem removes an item from order.items', () => {
+    const order = new Order();
+    console.log(order.items)
+    order.removeOrderedItem({itemName: 'item1'})
+    console.log(order.items)
+
+    expect(order.items).not.toContain('item');
+});
+
+test('retrieveOrderedItemByName returns an item from order.items', () => {
+    const order = new Order();
+    order.addOrderedItem({itemName: 'item1'})
+    order.addOrderedItem({itemName: 'item2'})
+    order.addOrderedItem({itemName: 'item3'})
+    const retrievedItem = order.retrieveOrderedItemByName('item2')
+    expect(retrievedItem).toEqual({itemName: 'item2'});
+});
+
+test(' incrementOrderTotal increases the order total', () => {
+    const order = new Order();
+    order.incrementOrderTotal(1.2,4)
+    expect(order.orderTotal).toEqual(4.8);
+});
+
+test(' decrementOrderTotal decreases the order total', () => {
+    const order = new Order();
+    order.decrementOrderTotal(1.2,4)
+    expect(order.orderTotal).toEqual(0);
+});
+
+
