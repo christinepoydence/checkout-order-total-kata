a3cd97e2aab5ba35d67d5ce8264ea4ab6535cc0f
Christine Poydence <christine.poydence@gmail.com>
2021-03-02 05:16:06 -0500

    added ability to calculate a markdown price in scannable items

--- src/classes/scannableItem.js
+++ src/classes/scannableItem.js
@@ -8,5 +8,20 @@ export default class ScannableItem {
         this.isMarkedDown = false;
         this.priceReduction = 0;
     }
+
+    markDownItem(priceReduction) {
+        this.isMarkedDown = true;
+        this.priceReduction = +priceReduction.toFixed(2);
+    };
+
+    removeMarkDown() {
+        this.isMarkedDown = false;
+        this.priceReduction = 0;
+    }
+
+    calculatePrice() {
+        return +(this.price - this.priceReduction).toFixed(2);
+    }
+
 }
 
--- src/classes/scannableItem.spec.js
+++ src/classes/scannableItem.spec.js
@@ -13,3 +13,53 @@ test('ScannableItem can be constructed', () => {
     expect(scannableItem.isMarkedDown).toBe(false);
     expect(scannableItem.priceReduction).toEqual(0);
 });
+
+test('Scannable item can be marked down', () => {
+    const item = {
+        itemName: 'kiwi',
+        unitType: 'ounces',
+        price: 1.42
+    }
+    const scannableItem = new ScannableItem(item);
+    scannableItem.markDownItem(0.10);
+    expect(scannableItem.isMarkedDown).toBe(true);
+    expect(scannableItem.priceReduction).toEqual(0.10);
+});
+
+test('Scannable item that are marked down can have the mark down removed', () => {
+    const item = {
+        itemName: 'kiwi',
+        unitType: 'ounces',
+        price: 1.42
+    }
+    const scannableItem = new ScannableItem(item);
+    scannableItem.markDownItem(0.10);
+    expect(scannableItem.isMarkedDown).toBe(true);
+    expect(scannableItem.priceReduction).toEqual(0.10);
+    scannableItem.removeMarkDown();
+    expect(scannableItem.isMarkedDown).toBe(false);
+    expect(scannableItem.priceReduction).toEqual(0);
+});
+
+test('Marked down items have the correct price calculation', () => {
+    const item = {
+        itemName: 'kiwi',
+        unitType: 'ounces',
+        price: 1.42
+    }
+    const scannableItem = new ScannableItem(item);
+    scannableItem.markDownItem(0.10);
+    expect(scannableItem.calculatePrice()).toEqual(1.32);
+    
+});
+
+test('Non-marked down items have the correct price calculation', () => {
+    const item = {
+        itemName: 'kiwi',
+        unitType: 'ounces',
+        price: 1.42
+    }
+    const scannableItem = new ScannableItem(item);
+    expect(scannableItem.calculatePrice()).toEqual(1.42);
+    
+});
