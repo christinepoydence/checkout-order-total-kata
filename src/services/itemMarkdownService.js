fac8953dc376acc9abd96e8d6e96a38839fa0e4a
Christine Poydence <christine.poydence@gmail.com>
2021-03-03 02:30:36 -0500

    add documentation to itemMarkdownService

--- src/services/itemMarkdownService.js
+++ src/services/itemMarkdownService.js
@@ -1,5 +1,11 @@
 import PointOfSale from '../classes/pointOfSale.js';
 
+/**
+ * Adds a markdown to a scannable item
+ * @param  {String} itemName       the name of the scannable item
+ * @param  {Number} priceReduction the amount to reduce the price by while the item is on markdown
+ * @throws {error}                 if itemName is not a valid scannableItem
+ */
 export const addMarkDownToItem = (itemName, priceReduction) => {
     const itemToMarkdown = PointOfSale.getInstance().retrieveScannableItemByName(itemName);
     if(!itemToMarkdown){
@@ -8,6 +14,11 @@ export const addMarkDownToItem = (itemName, priceReduction) => {
     itemToMarkdown.markDownItem(priceReduction);
 };
 
+/**
+ * Removes a markdown from a scannable item
+ * @param  {String} itemName       the name of the scannable item
+ * @throws {error}                 if itemName is not a valid scannableItem
+ */
 export const removeMarkDownFromItem = (itemName) => {
     const item = PointOfSale.getInstance().retrieveScannableItemByName(itemName);
     if(!item){
