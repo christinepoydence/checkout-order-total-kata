e1ae9272c4ef57d343d751eef6871a4207157047
Christine Poydence <christine.poydence@gmail.com>
2021-03-03 03:53:31 -0500

    aded documentation to orderedItem

--- src/classes/orderedItem.js
+++ src/classes/orderedItem.js
@@ -1,8 +1,13 @@
 import get from 'lodash.get';
 
+/** This is a class representing an ordered item */
 export default class OrderedItem {
+     /**
+     * Create an ordered item.
+     * @param {Object} item - An object containing the item's name, units ordered, and price
+     */
     constructor(item){
-        this.itemName = get(item, 'itemName').toLowerCase();
+        this.itemName = get(item, 'itemName', '').toLowerCase();
         this.units = get(item, 'units');
         this.price = +get(item,'price').toFixed(2);
     };
