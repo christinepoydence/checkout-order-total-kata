902711116b528401281ada3055c6d2f37851b987
Christine Poydence <christine.poydence@gmail.com>
2021-03-03 04:06:32 -0500

    add documentation to scannableItem

--- src/classes/scannableItem.js
+++ src/classes/scannableItem.js
@@ -1,6 +1,12 @@
 import get from 'lodash.get';
 
+/** Class representing a scannable item */
 export default class ScannableItem {
+
+    /**
+     * Create a ScannableItem
+     * @param {Object} item - An object containing the item's name, unit type, and price
+     */
     constructor(item){
         this.itemName = get(item, 'itemName').toLowerCase();
         this.unitType = get(item, 'unitType');
@@ -11,26 +17,44 @@ export default class ScannableItem {
         this.special = null;
     };
 
+    /**
+     * add a special deal to the ScannableItem
+     * @param {Object} special - The special to be added to the scannable item
+     */
     addSpecialToItem(special) {
         this.isOnSpecial = true;
         this.special = special;
     }
 
+    /**
+     * remove a special deal from the ScannableItem
+     * @param {Object} special - The special to be removed from the scannable item
+     */
     removeSpecialFromItem(special) {
         this.isOnSpecial = false;
         this.special = null;
     }
 
+    /**
+     * add a markdown to the ScannableItem
+     * @param {Number} priceReduction - The amount that the markdown reduces the unit price
+     */
     markDownItem(priceReduction) {
         this.isMarkedDown = true;
         this.priceReduction = +priceReduction.toFixed(2);
     };
 
+    /**
+     * remove a markdown from the ScannableItem
+     */
     removeMarkDown() {
         this.isMarkedDown = false;
         this.priceReduction = 0;
     };
 
+    /**
+     * calculate the price, inclusive of the full price and the markdown price
+     */
     calculatePrice() {
         return +(this.price - this.priceReduction).toFixed(2);
     };
--- src/classes/scannableItem.spec.js
+++ src/classes/scannableItem.spec.js
@@ -1,25 +1,23 @@
 import ScannableItem from './scannableItem.js';
 
+const item = {
+    itemName: 'kiwi',
+    unitType: 'ounces',
+    price: 1.42
+};
+
 test('ScannableItem can be constructed', () => {
-    const item = {
-        itemName: 'kiwi',
-        unitType: 'ounces',
-        price: 1.42
-    };
     const scannableItem = new ScannableItem(item);
     expect(scannableItem.itemName).toBe('kiwi');
     expect(scannableItem.unitType).toBe('ounces');
     expect(scannableItem.price).toBe(1.42);
     expect(scannableItem.isMarkedDown).toBe(false);
     expect(scannableItem.priceReduction).toEqual(0);
+    expect(scannableItem.special).toEqual(null);
+    expect(scannableItem.isOnSpecial).toBe(false);
 });
 
 test('Scannable item can be on special', () => {
-    const item = {
-        itemName: 'kiwi',
-        unitType: 'ounces',
-        price: 1.42
-    };
     const scannableItem = new ScannableItem(item);
     scannableItem.addSpecialToItem('special');
     expect(scannableItem.isOnSpecial).toBe(true);
@@ -27,11 +25,6 @@ test('Scannable item can be on special', () => {
 });
 
 test('Scannable item can be taken off special', () => {
-    const item = {
-        itemName: 'kiwi',
-        unitType: 'ounces',
-        price: 1.42
-    };
     const scannableItem = new ScannableItem(item);
     scannableItem.addSpecialToItem('special');
     expect(scannableItem.isOnSpecial).toBe(true);
@@ -42,23 +35,13 @@ test('Scannable item can be taken off special', () => {
 });
 
 test('Scannable item can be marked down', () => {
-    const item = {
-        itemName: 'kiwi',
-        unitType: 'ounces',
-        price: 1.42
-    };
     const scannableItem = new ScannableItem(item);
     scannableItem.markDownItem(0.10);
     expect(scannableItem.isMarkedDown).toBe(true);
     expect(scannableItem.priceReduction).toEqual(0.10);
 });
 
-test('Scannable item that are marked down can have the mark down removed', () => {
-    const item = {
-        itemName: 'kiwi',
-        unitType: 'ounces',
-        price: 1.42
-    };
+test('Scannable item can have the mark down removed', () => {
     const scannableItem = new ScannableItem(item);
     scannableItem.markDownItem(0.10);
     expect(scannableItem.isMarkedDown).toBe(true);
@@ -69,24 +52,12 @@ test('Scannable item that are marked down can have the mark down removed', () =>
 });
 
 test('Marked down items have the correct price calculation', () => {
-    const item = {
-        itemName: 'kiwi',
-        unitType: 'ounces',
-        price: 1.42
-    };
     const scannableItem = new ScannableItem(item);
     scannableItem.markDownItem(0.10);
     expect(scannableItem.calculatePrice()).toEqual(1.32);
-    
 });
 
 test('Non-marked down items have the correct price calculation', () => {
-    const item = {
-        itemName: 'kiwi',
-        unitType: 'ounces',
-        price: 1.42
-    };
     const scannableItem = new ScannableItem(item);
     expect(scannableItem.calculatePrice()).toEqual(1.42);
-    
 });
