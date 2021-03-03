ae7538695b5475b037682b1a2cf7be432b94d53c
Christine Poydence <christine.poydence@gmail.com>
2021-03-03 03:14:16 -0500

    refactor itemScanningService.spec for reability

--- src/services/itemScanningService.spec.js
+++ src/services/itemScanningService.spec.js
@@ -8,50 +8,56 @@ import {
     modifyScannableItemInSystem
 } from './itemEntryService.js';
 import { addMarkDownToItem, removeMarkDownFromItem } from "./itemMarkdownService";
+import OrderedItem from '../classes/orderedItem.js';
+import PointOfSale from '../classes/pointOfSale.js';
 
+const baseItem = {
+    itemName: 'fruit loops',
+    unitType: 'unit', 
+    price: 1.69
+};
+const priceReduction = 0.40;
+const expectedFinalPrice = baseItem.price - priceReduction;
 
 describe('scanItem', () => {
 
-    test('when a valid item is scanned, it is added to the list of scanned items', () => {
-        const baseItem = {itemName: 'pears', unitType: 'unit', price: 1.69};
+    beforeEach(() => {
+        Order.getInstance().items = [];
+        Order.getInstance().orderTotal = 0;
+        PointOfSale.getInstance().scannableItems = [];
         addScannableItemToSystem(baseItem);
+      });
+
+    test('when a valid item is scanned, it is added to the list of scanned items', () => {
         scanItem(baseItem.itemName);
-        expect(Order.getInstance().items).toContainEqual({itemName: 'pears', price: 1.69, units:1});
+        expect(Order.getInstance().items).toContainEqual({"itemName": baseItem.itemName, "price": baseItem.price, "units": 1});
     });
 
-    test('when a valid, marked-down item is scanned, it is added to the list of scanned items and the price reduction is accounted for in the order', () => {
-        const baseItem = {itemName: 'gingerbread', unitType: 'unit', price: 1.69};
-        addScannableItemToSystem(baseItem);
-        addMarkDownToItem(baseItem.itemName, 0.40);
+    test('when a valid, marked-down item is scanned, it is added to the list of scanned items', () => {
+        addMarkDownToItem(baseItem.itemName, priceReduction);
         scanItem(baseItem.itemName);
-        expect(Order.getInstance().items).toContainEqual({itemName: 'gingerbread', units: 1, price: 1.29});
+        expect(Order.getInstance().items).toContainEqual({itemName: baseItem.itemName, price: expectedFinalPrice, units: 1});
     });
 
     test('when a valid, marked-down item is scanned, the order total is correctly incremented', () => {
-        Order.getInstance().orderTotal = 0;
-        const baseItem = {itemName: 'ginger snaps', unitType: 'unit', price: 1.69};
-        addScannableItemToSystem(baseItem);
-        addMarkDownToItem(baseItem.itemName, 0.40);
+        addMarkDownToItem(baseItem.itemName, priceReduction);
         scanItem(baseItem.itemName);
-        expect(Order.getInstance().orderTotal).toEqual(1.29);
+        expect(Order.getInstance().orderTotal).toEqual(expectedFinalPrice);
     });
 
     test('when a valid item is scanned and it is already on the order, the units of the item on the order are incremented', () => {
-        const baseItem = {itemName: 'crackers', unitType: 'unit', price: 1.69};
-        addScannableItemToSystem(baseItem);
         scanItem(baseItem.itemName);
+        expect(Order.getInstance().items).toContainEqual({itemName: 'fruit loops', price: 1.69, units:1});
         scanItem(baseItem.itemName, 3);
-        expect(Order.getInstance().items).toContainEqual({itemName: 'crackers', price: 1.69, units:4});
+        expect(Order.getInstance().items).toContainEqual({itemName: 'fruit loops', price: 1.69, units:4});
     });
 
     test('when a valid item is passed to scanItem and it is already on the order, an error should be thrown if the price has changed', () => {
-        const baseItem = {itemName: 'soup', unitType: 'unit', price: 1.69};
-        addScannableItemToSystem(baseItem);
         scanItem( baseItem.itemName);
-        const updatedItem = {itemName: 'soup', unitType: 'unit', price: 1.79};
-        modifyScannableItemInSystem( updatedItem);
+        const updatedItem = {...baseItem, price: 1.79};
+        modifyScannableItemInSystem(updatedItem);
         expect(() => { scanItem(baseItem.itemName); }
-        ).toThrow(Error(`The price of the item has changed since the beginning of your order. Please remove all soup from your order and rescan them.`));
+        ).toThrow(Error(`The price of the item has changed since the beginning of your order. Please remove all fruit loops from your order and rescan them.`));
     });
 
     test('if an item that is not scannable is passed to scanItem, an error is thrown', () => {
@@ -60,35 +66,35 @@ describe('scanItem', () => {
     });
 
     test('when a valid item that is priced per unit is passed to scanItem, the price of that item is added to the total', () => {
-        Order.getInstance().orderTotal = 0;
-        const baseItem = {itemName: 'mango', unitType: 'pounds', price: 1.69};
-        addScannableItemToSystem(baseItem);
-        scanItem( 'mango');
-        expect(Order.getInstance().orderTotal).toEqual(1.69);
+        scanItem(baseItem.itemName);
+        expect(Order.getInstance().orderTotal).toEqual(baseItem.price);
     });
 
     test('when a valid item that is priced by weight is passed to scanItem, the price of that item is added to the total', () => {
-        Order.getInstance().orderTotal = 0;
-        const baseItem = {itemName: 'wheat bread', unitType: 'pounds', price: 1.69};
-        addScannableItemToSystem(baseItem);
-        scanItem(baseItem.itemName);
-        expect(Order.getInstance().orderTotal).toEqual(1.69);
+        const weightedItem = {itemName: 'wheat bread', unitType: 'pounds', price: 2.04};
+        addScannableItemToSystem(weightedItem);
+        scanItem(weightedItem.itemName);
+        expect(Order.getInstance().orderTotal).toEqual(2.04);
     });
 
 });
 
 describe('removeItemFromOrder', () => {
 
-    test('when a valid item is passed to removeItemFromOrder, it is removed from the list of items on the order', () => {
-        const baseItem = {itemName: 'barley', unitType: 'pounds', price: 1.69};
+    beforeEach(() => {
+        Order.getInstance().items = [];
+        Order.getInstance().orderTotal = 0;
+        PointOfSale.getInstance().scannableItems = [];
         addScannableItemToSystem(baseItem);
-        scanItem('barley');
-        expect(Order.getInstance().items).toContainEqual({itemName: 'barley', price: 1.69, units:1});
-        removeItemFromOrder('barley');
-        expect(Order.getInstance().items).not.toContainEqual({itemName: 'barley', price: 1.69, units:1});
+        scanItem(baseItem.itemName)
+      });
+
+    test('when a valid item is removed, it is removed from the list of items on the order', () => {
+        removeItemFromOrder(baseItem.itemName);
+        expect(Order.getInstance().items).not.toContainEqual(new OrderedItem(baseItem));
     });
 
-    test('when a valid item is passed to removeItemFromOrder, the quantity is reduced by the quantity of items that were removed', () => {
+    test('when a valid item is removed, the quantity is reduced by the quantity of items that were removed', () => {
         const baseItem = {itemName: 'turkey', unitType: 'pounds', price: 4.90};
         addScannableItemToSystem(baseItem);
         scanItem('turkey',5);
@@ -96,18 +102,15 @@ describe('removeItemFromOrder', () => {
         expect(Order.getInstance().items).toContainEqual({itemName: 'turkey', price: 4.90, units: 3});
     });
 
-    test('when a valid item is passed to removeItemFromOrder, the order total is reduced by the cost of that item', () => {
-        Order.getInstance().orderTotal = 3.00;
-        const baseItem = {itemName: 'fruit roll ups', unitType: 'unit', price: 0.50};
-        addScannableItemToSystem(baseItem);
-        scanItem('fruit roll ups');
-        removeItemFromOrder('fruit roll ups');
-        expect(Order.getInstance().orderTotal).toEqual(3.00);
+    test('when a valid item is removed, the order total is reduced by the cost of that item', () => {
+        expect(Order.getInstance().orderTotal).toEqual(1.69);
+        removeItemFromOrder('fruit loops');
+        expect(Order.getInstance().orderTotal).toEqual(0);
     });
 
-    test('when a valid item that is priced by weight is passed to removeItemFromOrder, the order total is reduced by the cost of that item', () => {
+    test('when a valid item that is priced by weight is removed, the order total is reduced by the cost of that item', () => {
         Order.getInstance().orderTotal = 3.00;
-        const baseItem = {itemName: 'lollipop', unitType: 'unit', price: 0.10};
+        const baseItem = {itemName: 'lollipop', unitType: 'pounds', price: 0.10};
         addScannableItemToSystem(baseItem);
         scanItem('lollipop',5);
         removeItemFromOrder('lollipop', 5);
@@ -115,7 +118,6 @@ describe('removeItemFromOrder', () => {
     });
 
     test('if a clerk tries to remove more items than were scanned, an error is thrown', () => {
-        Order.getInstance().orderTotal = 3.00;
         const baseItem = {itemName: 'tootsie rolls', unitType: 'unit', price: 0.10};
         addScannableItemToSystem(baseItem);
         scanItem('tootsie rolls',3);
@@ -124,7 +126,6 @@ describe('removeItemFromOrder', () => {
     });
 
     test('if an item not on the order is passed to removeItemFromOrder, an error is thrown', () => {
-        Order.getInstance().orderTotal = 3.00;
         const baseItem = {itemName: 'sprite', unitType: 'unit', price: 0.10};
         addScannableItemToSystem(baseItem);
         expect(() => {removeItemFromOrder('sprite'); }
@@ -132,7 +133,6 @@ describe('removeItemFromOrder', () => {
     });
 
     test('if an item that is not scannable is passed to removeItemFromOrder, an error is thrown', () => {
-        Order.getInstance().orderTotal = 3.00;
         expect(() => {removeItemFromOrder('M&Ms'); }
         ).toThrow(Error('M&Ms is not a valid item in this POS system.'));
     });
